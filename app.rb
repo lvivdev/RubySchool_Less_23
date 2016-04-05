d12f308565415fd843426931a71f63fc70ecc5a8
lvivdev <kl@alushta.net>
2016-04-05 16:05:12 +0300

    transfer 24 lesson to git of 23

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pony'
+
+
 
 configure do
   enable :sessions
@@ -11,7 +14,6 @@ helpers do
   def username
     session[:identity] ? session[:identity] : "#{@username}"
   end
-
 end
 
 before '/login/form/*' do
@@ -33,16 +35,13 @@ end
 post '/login/attempt' do
 	@username = params[:username]
 	@pass = params[:pass]
-
 	if @username == 'admin' && @pass == 'pass'
-				session[:identity] = @username
-				where_user_came_from = session[:previous_url] || '/'
-				redirect to where_user_came_from
-				
-	else erb :login_form
-
+		session[:identity] = @username
+		where_user_came_from = session[:previous_url] || '/'
+		redirect to where_user_came_from
+	else
+		erb :login_form
 	end
-
 end
 
 get '/about' do
@@ -54,39 +53,105 @@ get '/visit' do
 end
 
 post '/visit' do
-	@username = params[:username]
+	@user = params[:user]
 	@usermail = params[:usermail]
 	@userphone = params[:userphone]
 	@date_time = params[:date_time]
 	@barber = params[:barber]
 	@color = params[:color]
 
+	#create hash
+	hh = {
+		:user => 'Введите имя',
+		:usermail => 'Введите почту',
+		:userphone => 'Что-то не так с телефоном',
+		:date_time => 'Время тоже неверно'
+	}
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit		
+	end
 
 	f = File.open './public/users.txt', 'a'
-	f.write "Barber: #{@barber} for User: #{@username}, Mail: #{@usermail}, Phone: #{@userphone}, Date and time: #{@date_time}, Цвет стрижки: #{@color}"
+	f.write "Barber: #{@barber} for User: #{@user}, Mail: #{@usermail}, Phone: #{@userphone}, Date and time: #{@date_time}, Цвет стрижки: #{@color}"
 	f.close
 
-	erb "Будем ждать вас, #{@username.strip.capitalize} к #{@date_time}! Цвет: #{@color}"
+	Pony.mail({
+	:from => params[:user],
+    :to => 'klrealty.rs@gmail.com',
+    :subject => params[:user] + " has contacted you via the Website",
+    :body => "Name: " + params[:user] + " " + "Mail: " + params[:usermail] + " " +  "Phone: " + params[:userphone] + " " +  "Date and time: " + params[:date_time] + " " +  "Barber: " + params[:barber] + " " +  "Color: " + params[:color],
+    :via => :smtp,
+    :via_options => {
+     :address              => 'smtp.gmail.com',
+     :port                 => '587',
+     :enable_starttls_auto => true,
+     :user_name            => 'klrealty.rs@gmail.com',
+     :password             => '81caeb71a2019fbb7ada016b85d040de',
+     :authentication       => :login, 
+     :domain               => "localhost.localdomain" 
+     }
+    })
+    redirect '/success' 
 end
 
 get '/contacts' do
-	erb :contacts
+	erb :contacts, :layout => :layout
 end
 
 post '/contacts' do
-	@user_name = params[:user_name]
-	@user_mail = params[:user_mail]
-	@user_message = params[:user_message]
+
+	@name = params[:name]
+	@mail = params[:mail]
+	@message = params[:message]
+
+	#create hash to define error messages
+	hash = {
+		:name => 'Введите имя',
+		:mail => 'Введите почту',
+		:message => 'Введите сообщение'
+	}
+
+	#присвоить переменной error значение value из массива hash. Выводить все ошибки.
+	@error = hash.select{|key,_| params[key] == ''}.values.join(", ")
+
+	if @error != ''
+		return erb :contacts
+	end
 
 	c = File.open './public/contacts.txt', 'a'
-	c.write "User: #{@user_name}, Mail: #{@user_mail}, Message: #{@user_message} "
+	c.write "User: #{@name}, Mail: #{@mail}, Message: #{@message} "
 	c.close
 
-	erb "Сообщение отправлено! Мы свяжемся с вами."
+	Pony.mail({
+	:from => params[:name],
+    :to => 'klrealty.rs@gmail.com',
+    :subject => params[:name] + " has contacted you via the Website",
+    :body => "Сообщение: " + " " + params[:message] + " " + " Почта: " + params[:mail],
+    :via => :smtp,
+    :via_options => {
+     :address              => 'smtp.gmail.com',
+     :port                 => '587',
+     :enable_starttls_auto => true,
+     :user_name            => 'klrealty.rs@gmail.com',
+     :password             => '81caeb71a2019fbb7ada016b85d040de',
+     :authentication       => :login, 
+     :domain               => "localhost.localdomain" 
+     }
+    })
+    redirect '/success' 
+end
+
+get('/success') do
+	erb "Спасибо за обращение!"
 end
 
 get '/logout' do
   session.delete(:identity)
   erb "<div class='alert alert-message'>Logged out</div>"
 end
+
+
 
